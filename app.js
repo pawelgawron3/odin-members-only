a47a5776bd6ea87ecd499d7618ccac292a86337a
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-09-16 12:30:42 +0200

    feat: configure EJS view engine

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
