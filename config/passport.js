d7018153619fd7d59bd8c77ae7bd006dd7d232e2
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-09-16 14:51:26 +0200

    feat: configure local passport strategy

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,29 @@
 import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
 import { db } from "../db/queries.js";
+import { comparePassword } from "../utils/password.js";
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await db.getUserByUsername(username);
+
+      if (!user) {
+        return done(null, false);
+      }
+
+      const isValid = await comparePassword(password, user.password);
+
+      if (!isValid) {
+        return done(null, false);
+      }
+
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  }),
+);
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
@@ -7,8 +31,7 @@ passport.serializeUser((user, done) => {
 
 passport.deserializeUser(async (id, done) => {
   try {
-    const user = await db.getUser(id);
-
+    const user = await db.getUserById(id);
     done(null, user);
   } catch (err) {
     done(err);
