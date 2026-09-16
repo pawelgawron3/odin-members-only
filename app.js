de466c19280e764fd658c3d10f550da2a89f8534
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-09-16 12:18:15 +0200

    feat: add basic Express server setup

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+
+  console.log(`Server is listening for requests on port ${PORT}`);
+});
