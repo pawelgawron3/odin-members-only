cf4080cd44ef023fde902a755d39ac97006a1124
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-09-18 13:41:08 +0200

    feat: import requireAuth function and use it

--- routes/messagesRouter.js
+++ routes/messagesRouter.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
+import requireAuth from "../utils/requireAuth.js";
 import messagesController from "../controllers/messagesController.js";
 
 const messagesRouter = Router();
 
-messagesRouter.get("/new", messagesController.getNewMessageForm);
+messagesRouter.get("/new", requireAuth, messagesController.getNewMessageForm);
+messagesRouter.post("/new", (req, res) => {
+  res.send("Good");
+});
 
 export default messagesRouter;
