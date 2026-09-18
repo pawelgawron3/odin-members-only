24e157b8bf11d17cf803c6c6317f0adbc1f631e7
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-09-18 12:29:55 +0200

    feat: import and use formatDate function

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,10 +1,11 @@
 import { db } from "../db/queries.js";
+import formatDate from "../utils/formatDate.js";
 
 const homeController = {
   async getHomepage(req, res) {
     const messages = await db.getMessages();
 
-    res.render("home", { messages });
+    res.render("home", { messages, formatDate });
   },
 };
 
