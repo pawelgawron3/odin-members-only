c120d5d4f237e7538cb78cb7f3653f88fa32e517
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-09-19 10:19:27 +0200

    feat: add createNewMessage controller function

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -4,6 +4,19 @@ const messagesController = {
   getNewMessageForm(req, res) {
     res.render("new-message");
   },
+
+  async createNewMessage(req, res) {
+    try {
+      const userId = req.user.id;
+      const message = req.body;
+
+      await db.addMessage(userId, message);
+
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+    }
+  },
 };
 
 export default messagesController;
