8e949dd66e5b79509ce78ac51f93bc4cf7c88dcf
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-09-20 10:39:42 +0200

    feat: add requireAdminAuth function

--- utils/requireAuth.js
+++ utils/requireAuth.js
@@ -1,4 +1,4 @@
-function requireAuth(req, res, next) {
+export function requireAuth(req, res, next) {
   if (!req.isAuthenticated()) {
     return res.redirect("/auth/login");
   }
@@ -6,4 +6,10 @@ function requireAuth(req, res, next) {
   next();
 }
 
-export default requireAuth;
+export function requireAdminAuth(req, res, next) {
+  if (!req.user.is_admin) {
+    return res.redirect("/");
+  }
+
+  next();
+}
